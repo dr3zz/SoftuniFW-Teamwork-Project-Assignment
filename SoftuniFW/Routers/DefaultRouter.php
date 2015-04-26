4aaad6c9b8249d63ac0850da8ea88c660a5011e0
dr3zz <dr3zz@abv.bg>
2015-04-26 13:53:16 +0300

    router

--- SoftuniFW/Routers/DefaultRouter.php
+++ SoftuniFW/Routers/DefaultRouter.php
@@ -1,9 +1,29 @@
 <?php
 
 namespace SoftUniFw\Routers;
-class DefaultRouter {
+class DefaultRouter
+{
 
-    public function parse(){
-      echo '<pre>'.print_r($_SERVER,true).'</pre>';
+    public function parse()
+    {
+
+        $_uri = substr($_SERVER['PHP_SELF'], strlen($_SERVER['SCRIPT_NAME']) + 1);
+        $controller = null;
+        $method = null;
+        $_params = explode('/', $_uri);
+        if ($_params[0]) {
+            $controller .= ucfirst($_params[0]);
+
+            if ($_params[1]) {
+                $method = $_params[1];
+                unset($_params[0], $_params[1]);
+            } else {
+                $method = 'index';
+            }
+        } else {
+            $controller = 'index';
+            $method = 'index';
+        }
+        echo $controller . '<br>' . $method;
     }
 }
