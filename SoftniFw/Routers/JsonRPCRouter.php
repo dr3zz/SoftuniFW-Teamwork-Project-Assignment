358d5f629fd325e0636a313eeda309c0eae23cdf
dr3zz <dr3zz@abv.bg>
2015-04-30 22:59:12 +0300

    update jsonRPCRouter

--- SoftniFw/Routers/JsonRPCRouter.php
+++ SoftniFw/Routers/JsonRPCRouter.php
@@ -6,17 +6,52 @@ namespace SoftUniFW\Routers;
 
 class JsonRPCRouter implements \SoftUniFW\Routers\IRouter
 {
+    private $_map = array();
+    private $_requestId;
+
     public function __construct()
     {
         if ($_SERVER['REQUEST_METHOD'] != 'POST' ||
-            empty($_SERVER['CONTENT_TYPE']) || $_SERVER['CONTENT_TYPE'] != 'application/json') {
+            empty($_SERVER['CONTENT_TYPE']) || $_SERVER['CONTENT_TYPE'] != 'application/json'
+        ) {
             throw new \Exception('Require json request', 400);
+        }
+    }
+
+    public function setMethodsMap($ar)
+    {
+        if (is_array($ar)) {
+            $this->_map = $ar;
         }
     }
 
     public function getURI()
     {
+        if (!is_array($this->_map) || count($this->_map) == 0) {
+            $ar = \SoftUniFW\App::getInstance()->getConfig()->rpcRoutes;
+            if (is_array($ar) && count($ar) > 0) {
+                $this->_map = $ar;
+            } else {
+                throw new \Exception('Router require method map', 500);
+            }
+        }
 
+        $request = json_decode(file_get_contents('php://input'), true);
+        if (is_array($request) || !isset($request['method'])) {
+            throw new \Exception('Require json request', 400);
+        } else {
+            if ($this->_map[$request['method']]) {
+                $this->_requestId = $request['id'];
+                return $this->_map[$request['method']];
+            } else {
+                throw new \Exception('Method not found', 501);
+            }
+
+        }
     }
 
+    public function getRequestId()
+    {
+        return $this->_requestId;
+    }
 }
